f7f35d58933bd0968aa7934b5553f40ef7ebc671
Yann Herklotz <git@yannherklotz.com>
2020-09-04 13:33:30 +0100

    Add code for straight line

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Alluvial
 
 A library for generating alluvial diagrams, currently using a latex and tikz backend.
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+newtype Ribbon = Ribbon [Bool] Int
+
+newtype RawData = RawData [Ribbon]
+
+newtype Limits = Limits { limitsLow :: Int, limitsHigh :: Int }
+
+drawSegment :: Limits -> Bool -> Bool -> Int -> String
+drawSegment (Limits _ h) True True i =
+  show (i,h) <> " -- " <> show ((fromInteger i + 1.0) / 2.0, h) <> " -- " show (i+1,h)
+
+drawRibbon :: Ribbon -> String
+drawRibbon (Ribbon b i) =
+
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
